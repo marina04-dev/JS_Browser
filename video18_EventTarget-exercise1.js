d68d777daea5c3738eeaf29245af85fd943c0939
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-08 01:26:29 -0800

    Create video18_EventTarget-exercise1.js

---
+++ video18_EventTarget-exercise1.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello World!</title>
+    <link rel="stylesheet" href="styles.css" />
+    
+  </head>
+  <body>
+      <button>Add Item</button>
+      <button>Remove Item</button>
+      <ul>
+        <li>Item 1</li>
+      </ul>
+      <script>
+        "use strict"
+        let button1 = document.querySelector("button:first-of-type");
+        let button2 = document.querySelector("button:last-of-type");
+        let list = document.querySelector("ul");
+        let item = document.querySelector("li");
+        
+        button1.addEventListener("click", ()=>{
+          let newItem = item.cloneNode();
+          newItem.textContent = `Item ${list.children.length + 1}`;
+          list.appendChild(newItem);
+        });
+        button2.addEventListener("click", ()=>{
+          if (list.children.length > 1) {
+            let itemToRemove = document.querySelector("li:last-of-type");
+            list.removeChild(itemToRemove);
+          }
+        });
+
+    </script>
+  </body>
+</html>
