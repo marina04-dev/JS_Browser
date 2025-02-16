acd688ce369fcdd89453d60d136d28e667ffb331
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-16 10:12:52 -0800

    Create video32_exercise2.js

---
+++ video32_exercise2.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" id="input" value="Some Text">
+    <button id="btn">Click me</button>
+    <button id="btn2">Click me 2</button>
+    <script>
+        let input = document.getElementById('input');
+        let button1 = document.getElementById('btn');
+        let button2 = document.getElementById('btn2');
+        button1.addEventListener('click', function() {
+            let content = input.getAttribute('value');
+            console.log(content);
+        });
+
+        button2.addEventListener('click', function() {
+            let content = input.value;
+            console.log(content);
+        });
+
+    </script>
+</body>
+</html>
